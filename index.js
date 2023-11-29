04d4b6d3b604893c8d56170880dc902f73c1db63
Elazar <elazaram291@gmail.com>
2023-11-30 01:18:58 +0200

    30.11.23

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { default: mongoose } = require('mongoose');
-const UserModel = require('./models/user');
+const UserModel = require('./models/User.js');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const app = express();
